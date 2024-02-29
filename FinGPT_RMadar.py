d2b9ddf837b32cc5ea5b9db9a6e5579f55835377
Ryan Madar <rmadar7@gmail.com>
2024-02-29 18:35:42 -0500

    Update FinGPT_RMadar.py
    Commented out quantization

--- FinGPT_RMadar.py
+++ FinGPT_RMadar.py
@@ -163,18 +163,18 @@ training_args = TrainingArguments(
 
     )
 
-q_config = BitsAndBytesConfig(load_in_4bit=True,
+"""q_config = BitsAndBytesConfig(load_in_4bit=True,
                                 bnb_4bit_quant_type='nf4',
                                 bnb_4bit_use_double_quant=True,
                                 bnb_4bit_compute_dtype=torch.float16
-                                )
+                                )"""
 
 
 model_name = "THUDM/chatglm2-6b"
 tokenizer = AutoTokenizer.from_pretrained(model_name, trust_remote_code=True)
 model = AutoModel.from_pretrained(
         model_name,
-        quantization_config=q_config,
+        #quantization_config=q_config,
         trust_remote_code=True,
         device='cuda'
     )
@@ -293,3 +293,4 @@ trainer.train()
 writer.close()
 # save model
 model.save_pretrained(training_args.output_dir)
+
